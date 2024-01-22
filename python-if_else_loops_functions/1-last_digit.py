6ec8b513247553fcd6fca34352222cace6e98b5d
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-22 06:14:49 -0800

    test 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number > 0:
     lastdigit = int(strnum[-1])
 elif number < 0:
     lastdigit = -1 * int(strnum[-1])
-print(f"Last digit of {number} is {lastdigit}", end = " ")
+print(f"Last digit of {number} is {lastdigit}", end=" ")
 if lastdigit > 5:
     print("and is greater than 5")
 elif lastdigit == 0:
