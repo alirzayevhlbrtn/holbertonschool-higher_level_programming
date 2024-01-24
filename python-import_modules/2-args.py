609eafe887b12ca41700ed409b7f93d089d6af04
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-24 05:41:41 -0800

    test 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(length))
     for i in ramge(length):
-        print("{}: {}".formati(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
