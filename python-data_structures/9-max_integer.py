940763ac6de80c263d29bbe5add9a20d543d03e0
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-26 03:50:47 -0800

    test 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        mx = 0
+        for v in my_list:
+            if v > max:
+                v = max
+        return mx
+    return None
