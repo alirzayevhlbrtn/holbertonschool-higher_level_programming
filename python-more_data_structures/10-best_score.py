304e482883415b814a92623d79f3d6a8a4412725
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-30 03:43:31 -0800

    test 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if !a_dictionary:
+    if a_dictionary:
         for i in a_dictionary.keys():
             if a_dictionary[i] == max(a_dictionary.values()):
                 return i
