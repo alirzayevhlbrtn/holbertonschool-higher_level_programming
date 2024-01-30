f904246500c25f799e249aa681a9fe82357eb589
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-30 03:28:38 -0800

    test 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary.keys():
-        if i == key:
-            a_dictionary.pop(key)
+    if i in a_dictionary.keys():
+        a_dictionary.pop(key)
     return a_dictionary
