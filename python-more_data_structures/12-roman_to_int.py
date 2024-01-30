0c635b4ce5919a2afa7fa9610102a153a3740239
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-30 04:10:34 -0800

    test 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dic = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     i = 0
     num = 0
     s = roman_string
     if roman_string:
         while i < len(s):
-            if i + 1 < len(s) and s[i : i + 2] in dic:
-                num += dic[s[i : i + 2]]
+            if i + 1 < len(s) and s[i: i + 2] in dic:
+                num += dic[s[i: i + 2]]
                 i += 2
             else:
                 num += dic[s[i]]
