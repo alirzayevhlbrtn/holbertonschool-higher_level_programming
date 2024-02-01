5727664057f7d033cb699d10da46a943cc6ab922
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-01 04:06:23 -0800

    test 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defining a class Square with properties
+"""
+
+
+class Square:
+    """
+    Class Square
+    """
+    def __init__(self, size) -> None:
+        """
+        __init__ method that sets the sets the size of the square
+        
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
