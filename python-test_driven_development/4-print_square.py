8d80dc238f8da38efb13789a4996f151c4bff562
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-04 05:49:27 -0800

    3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+
 """
-Square printer
+Menasiz xetalar toplusu
 """
 
 
 def print_square(size):
     """
-    function of printer
+    Print a square
     """
 
-    if not isinstance(size, (int, float)):
+    if not isinstance(size, int) or (isinstance(size, float) & size < 0):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        for j in range(size):
+    for _ in range(size):
+        for _ in range(size):
             print("#", end="")
         print()
