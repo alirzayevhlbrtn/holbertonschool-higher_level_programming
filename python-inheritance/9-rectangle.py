3c89be2a2db138411a09c7df63ea350e8c28df5a
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-06 02:24:46 -0800

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Document of empty class
+"""
+
+
+BaseGeometry = __import__('8-rectangle').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class of inherited rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
