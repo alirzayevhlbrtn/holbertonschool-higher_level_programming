0e0eb7acb39a28b9d2bab333d64e0b580c10bcee
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-06 01:32:08 -0800

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Document of empty class
+"""
+
+
+class BaseGeometry:
+    """
+    class of BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
