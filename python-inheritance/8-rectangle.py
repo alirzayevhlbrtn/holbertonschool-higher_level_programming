d40a89cf24f0ae1fcdace2a2f1a4f21b13adec3d
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-06 02:06:47 -0800

    8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Document of empty class
+"""
+
+
+class BaseGeometry:
+    """
+    class of BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    Class of inherited rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
