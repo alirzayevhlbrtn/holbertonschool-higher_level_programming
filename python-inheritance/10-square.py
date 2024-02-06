d3a26e58c32530f559fea7236f9e0c989d7d0c7b
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-06 02:32:49 -0800

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Document of empty class
+"""
+
+
+BaseGeometry = __import__('9-rectangle').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    Class of inherited rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
