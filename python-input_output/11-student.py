ee799780a9aa3b452e86e7445da1cad27139f261
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-08 02:54:20 -0800

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Student to json
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            res = {}
+            for att in attrs:
+                if att in self.__dict__:
+                    res[att] = self.__dict__[att]
+            return res
+
+    def reload_from_json(self, json):
+        for key in self.__dict__.keys():
+            self.__dict__[key] = json[key]
