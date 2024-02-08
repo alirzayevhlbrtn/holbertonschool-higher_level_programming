8bd38eea4848a2a707937b250f61a50be8b2785d
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-08 01:12:35 -0800

    5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     function save
     """
     with open(filename, 'w', encoding="utf-8") as jf:
-        jf.dump(my_obj, jf)
+        json.dump(my_obj, jf)
