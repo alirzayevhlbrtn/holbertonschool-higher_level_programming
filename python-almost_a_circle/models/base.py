fb1b5e34ac70d6c6de6d127bef30ea28f1c7aab2
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-12 00:36:08 -0800

    1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Base class
+"""
+
+
+class Base:
+    """
+    Class of Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
