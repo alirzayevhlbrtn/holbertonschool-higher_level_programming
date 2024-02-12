226b2271526a9a5564123385cd7dca333c36ebc1
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-12 02:37:40 -0800

    2

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,3 +38,15 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        dictionary of square
+        """
+        square_dict = {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y,
+        }
+        return square_dict
