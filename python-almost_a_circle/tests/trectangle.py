5a73d3739de3d0bfebe2f0a1dd7b066ec3377105
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-02-13 00:17:56 -0800

    partial test

---
+++ python-almost_a_circle/tests/trectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Test for Rectangle
+"""
+import os
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def t
